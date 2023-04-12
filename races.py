d10bc506167e6cfd41404e708401e2da51093bdd
River McCubbin <mccubbinri@gmail.com>
2023-04-12 11:15:11 -0400

    updated races to include old race (which are apparently still in use!)

--- races.py
+++ races.py
@@ -14,33 +14,44 @@ soup = BeautifulSoup(page.text, "html.parser")
 # find tables
 tables = soup.findAll("table")
 
-# get the specific data table with race info
-table = tables[1]
+# prep a dictionary for organization
+raceDict = {}
 
-# collect data from each row of the data table
-for row in table.tbody.find_all("tr"):
+# collect data from each row of the data tables
+for table in (tables[1], tables[2]):
+    print("\n\n\nnew table\n\n\n")
+    for row in table.tbody.find_all("tr"):
 
-    # get each cell in the row
-    cells = row.find_all("td")
+        race = {}
 
-    if (len(cells) == 4):
-        # get race title
-        title = cells[0].text.strip()
+        # get each cell in the row
+        cells = row.find_all("td")
 
-        # get elements
-        elements = "\"" + ", ".join([a['title'].strip() for a in cells[1].find_all("a")]) + "\""
-        
-        # get recommended dragons
-        recommendedDragons = "\"" + ", ".join([a.text.strip() for a in cells[2].find_all("a")]) + "\""
+        if (len(cells) == 4):
+            # get race title
+            title = cells[0].text.strip()
 
-        # get perfect dragons
-        if (cells[3].find_all("a") == []):
-            perfectDragons = cells[3].text.strip()
-        else:
-            perfectDragons = "\"" + ", ".join([a.text.strip() for a in cells[3].find_all("a")]) + "\""
+            # construct race dictionary
+            # get elements
+            race["Elements"] = "\"" + ", ".join([a['title'].strip() for a in cells[1].find_all("a")]) + "\""
+            
+            # get recommended dragons
+            race["Recommended Dragons"] = "\"" + ", ".join([a.text.strip() for a in cells[2].find_all("a")]) + "\""
 
-        # write to file
-        raceFile.write(f"{title}, {elements}, {recommendedDragons}, {perfectDragons}\n")
-        #print(f"the recommended dragons for {title} are {elements} dragons including {recommendedDragons} and the perfect dragons are {perfectDragons}")
+            # get perfect dragons
+            if (cells[3].find_all("a") == []):
+                race["Perfect Dragons"] = cells[3].text.strip()
+            else:
+                race["Perfect Dragons"] = "\"" + ", ".join([a.text.strip() for a in cells[3].find_all("a")]) + "\""
+
+            # add race to dictionary if it hasn't been added by the newer table
+            if title not in raceDict:
+                raceDict[title] = race
+
+# once the dictionary is complete, write the contents to file
+for race in sorted(list(raceDict.keys())):
+    # write to file
+    raceFile.write(f"{race}, {raceDict[race]['Elements']}, {raceDict[race]['Perfect Dragons']}, {raceDict[race]['Recommended Dragons']}\n")
+    #print(f"the recommended dragons for {title} are {elements} dragons including {recommendedDragons} and the perfect dragons are {perfectDragons}")
 
 raceFile.close()
